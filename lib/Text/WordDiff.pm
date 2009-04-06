1f232c8e388a261326149c34f6d0992b6a2f2a7c
David E. Wheeler <david@kineticode.com>
2009-04-06 18:39:35 +0000

    Updated copyright dates.

--- lib/Text/WordDiff.pm
+++ lib/Text/WordDiff.pm
@@ -597,7 +597,7 @@ David Wheeler <david@kineticode.com>
 
 =head1 Copyright and License
 
-Copyright (c) 2005-2008 David Wheeler. Some Rights Reserved.
+Copyright (c) 2005-2009 David Wheeler. Some Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
